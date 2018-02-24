484be5c424d1043f5980e9318d6ccd0fa95cc4c7
npontet <nicopontet@gmail.com>
2018-02-24 00:00:30 -0300

    Conexion a BD y login

---
+++ Obligatorio/config/configuracion.php
@@ -0,0 +1,9 @@
+<?php
+
+define("MOTOR","mysql");
+define("SERVIDOR","localhost");
+define("BASEDATOS","obligatorio");
+define("USUARIOBASE","root");
+define("CLAVEBASE","root");
+
+?>
--- Obligatorio/pages/login.php
+++ Obligatorio/pages/login.php
@@ -1,20 +1,41 @@
 <?php
 session_start();
+require_once("../includes/class.Conexion.BD.php");
+require_once("../config/configuracion.php");
+
+
+$conn = new ConexionBD(MOTOR, SERVIDOR, BASEDATOS, USUARIOBASE, CLAVEBASE);
 
 $usuario = $_POST['txtUsuario'];
 $clave = $_POST['txtClave'];
+if ($conn->conectar()) {
+    $sql = "SELECT * FROM Usuarios WHERE usuUsuario = :usu";
+    //cargo los parametros para la sql
+    $parametros = array();
+    $parametros[0] = array("usu",$usuario,"string");
+    //ejecuto la consulta
+    if($conn->consulta($sql,$parametros)){
+        
+        $fila=$conn->siguienteRegistro();
+        if(md5($clave) == $fila['usuClave']){
+        $_SESSION['ingreso'] = true;
+        $_SESSION['usuario'] = $usuario;
+        setcookie("txtUsu",$usuario,time()+(60*60*24));
+        header("Location: ../inicioPrivado.php");
+        }
+        else{
+            $_SESSION['ingreso'] = false; 
+            $_SESSION['mensaje'] = "Usuario o clave erronea!";
+            header("Location: ../index.php");
+        }
+    }
+    else {
+            echo "Error de Consulta " . $conn->ultimoError(); 
+    }
+} else {
+    echo "Error de conexión de Base de datos. " . $conn->ultimoError();;
+}
 
-if($usuario == "admin" && $clave == "1234"){
-    $_SESSION['ingreso'] = true;
-    $_SESSION['usuario'] = $usuario;
-    setcookie("txtUsu",$usuario,time()+(60*60*24));
-    header("Location: ../inicioPrivado.php");
-}
-else{
-    $_SESSION['ingreso'] = false; 
-    $_SESSION['mensaje'] = "Usuario o clave erronea!";
-    header("Location: index.php");
-}
 
 
 ?>
